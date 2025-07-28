993c4e0f40663ddea051ff3a1de28be9a3bd2a9c
Isha Dijcks <ishadijcks@gmail.com>
2025-07-28 17:19:47 +0200

    Add phone number

--- src/lib/components/IshaFooter.svelte
+++ src/lib/components/IshaFooter.svelte
@@ -6,6 +6,7 @@
   <nav>
     <h6 class="footer-title">Contact</h6>
     <a class="text-primary" href="mailto:{EMAIL}">{EMAIL}</a>
+    <span class="text-primary">Tel. 06-34704820</span>
     <span class="text-primary">KvK 94042675 The Hague</span>
   </nav>
 </footer>
